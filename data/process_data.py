0ae79bf808a54a944744fa042341694f9aa9473d
thijsessens <t.c.essens@gmail.com>
2020-09-30 17:06:45 -0700

    Update process_data.py
    Adding if_exists='replace' parameter to df.to_sql() function

--- data/process_data.py
+++ data/process_data.py
@@ -41,7 +41,7 @@ def save_data(df, database_filename):
     '''this function saves the data on the sqlite database'''
 
     engine = create_engine('sqlite:///'+database_filename)
-    df.to_sql('data', engine, index=False)  
+    df.to_sql('data', engine, index=False, if_exists='replace')  
 
 
 def main():
